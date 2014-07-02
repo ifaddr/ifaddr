e52e20929bd8842d3b8534f110cf438a64bf0d53
stefan <stefan@centos.(none)>
2014-07-02 13:24:20 +0200

    OS selection

--- ifaddr.py
+++ ifaddr.py
@@ -1,7 +1,13 @@
 # Copyright (C) 2014 Stefan C. Mueller
 
 
-from ifaddr_win32 import enumerate_interfaces
+import os
 
+if os.name == "nt":
+    from ifaddr_win32 import enumerate_interfaces
+elif os.name == "posix":
+    from ifaddr_posix import enumerate_interfaces
+else:
+    raise RuntimeError("Unsupported Operating System: %s" % os.name)
 
 print "\n".join(map(str,enumerate_interfaces()))
