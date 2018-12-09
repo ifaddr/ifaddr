48451f5140af9b723cf2e59635978f75f2e9bad4
Kevin Schlosser <drschlosser@hotmail.com>
2018-12-09 02:37:34 -0700

    fixes installation of ipaddress module
    A user had reported that the install_requires= line of the setup causes
    issues #14. This update changes it so that the testing of the python
    version happens in the ifaddr package and not in distutuls. This is a
    far better way to go about it due to the very large number of distutils
    and setup tools versions available. not all versions support the same
    syntax or the install_requires as well. but all versions do
    support requires= with a string for the module name or an empty list
    if there are no dependencies. This will remove the need to add special
    lines to handle specifically the setup tools version. This package has a
    pretty simple setup routine, so there is no need to have requirements for
    specific setup tools versions.

--- setup.py
+++ setup.py
@@ -6,18 +6,19 @@
 # rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 # sell copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
+import sys
 import os.path
 from setuptools import setup, find_packages
 
@@ -36,5 +37,5 @@ setup(
     author_email='scm@smurn.org',
     url='https://github.com/pydron/ifaddr',
     packages = find_packages(),
-    install_requires = ['ipaddress;python_version<"3.3"'],
+    requires = ['ipaddress'] if sys.version_info[:2] < (3, 3) else [],
 )
