2960a3f53bbbcc61547d4f5412041971c2763101
Jakub Stasiak <jakub@stasiak.at>
2022-06-15 21:05:32 +0200

    Drop Python 3.6 compatibility, it reached end-of-life already

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'Topic :: System :: Networking',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
