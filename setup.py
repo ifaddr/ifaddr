c62745a66b24b5d1038b124b0ed345f0c8f08afb
Jakub Stasiak <jakub@stasiak.at>
2022-06-15 23:20:31 +0200

    Mention support for Python 3.9 and 3.10 in the package metadata (#52)

--- setup.py
+++ setup.py
@@ -45,6 +45,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     keywords=['network interfaces', 'network adapters', 'network addresses', 'IP addresses'],
 )
