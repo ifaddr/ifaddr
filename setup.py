784f0ee7f885c0522e97086bd8edbd587125c4d2
Stefan Agner <stefan@agner.ch>
2024-04-30 08:47:22 +0200

    Drop Python 3.7 from classifiers
    Support for Python 3.7 has officially been removed from the library,
    hence remove it from the classifiers list.

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         'Topic :: System :: Networking',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
